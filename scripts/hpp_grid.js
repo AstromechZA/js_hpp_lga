d25092182ae7711a64c0e0d3cf849031ee79e976
AstromechZA <benmeier42@gmail.com>
2014-04-01 17:54:40 +0200

    made grid datastructure

---
+++ scripts/hpp_grid.js
@@ -0,0 +1,101 @@
+var HPPGRID = function _HPPGRID(width, height) {
+	this.width = width;
+	this.height = height;
+
+	this.grid = new Uint16Array(this.width * this.height);
+}
+
+// Get the numeric value (4 bits) of the cell ANDED by the val
+HPPGRID.prototype.get_v = function(x, y, p) {
+	var _x = Math.floor(x / 2);
+	var _y = Math.floor(y / 2);
+
+	var _ox = x % 2;
+	var _oy = y % 2;
+
+	// 16 bit cell value
+	var _v = this.grid[_y * this.width + _x];
+
+	var _shift = 4 * (_oy * 2 + _ox);
+
+	var _p = p & 15;
+	return (_v >> _shift) & _p;
+};
+
+HPPGRID.prototype.get_up = function(x, y) {
+	return (this.get_v(x, y, 8) > 0);
+};
+
+HPPGRID.prototype.get_right = function(x, y) {
+	return (this.get_v(x, y, 4) > 0);
+};
+
+HPPGRID.prototype.get_down = function(x, y) {
+	return (this.get_v(x, y, 2) > 0);
+};
+
+HPPGRID.prototype.get_left = function(x, y) {
+	return (this.get_v(x, y, 1) > 0);
+};
+
+// Set the numeric value (4 bits) of the cell
+HPPGRID.prototype.set_v = function(x, y, p, bool) {
+	var _x = Math.floor(x / 2);
+	var _y = Math.floor(y / 2);
+
+	var _ox = x % 2;
+	var _oy = y % 2;
+
+	var A = this.grid[_y * this.width + _x];
+
+	var B = p & 15;
+
+	var C = 4 * (_oy * 2 + _ox);
+	var D = (A - (A & (B << C)));
+	if (bool) D = D |(B << C);
+	this.grid[_y * this.width + _x] = D;
+};
+
+HPPGRID.prototype.set_up = function(x, y, b) {
+	this.set_v(x, y, 8, b);
+};
+
+HPPGRID.prototype.set_right = function(x, y, b) {
+	this.set_v(x, y, 4, b);
+};
+
+HPPGRID.prototype.set_down = function(x, y, b) {
+	this.set_v(x, y, 2, b);
+};
+
+HPPGRID.prototype.set_left = function(x, y, b) {
+	this.set_v(x, y, 1, b);
+};
+
+HPPGRID.prototype.get_density = function(x, y) {
+	var v = this.get_v(x, y, 15);
+	return [0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4][v];
+};
+
+HPPGRID.prototype.print = function() {
+	var s = '';
+	for (var y = 0; y < this.height*2; y++) {
+		for (var x = 0; x < this.width*2; x++) {
+			if (x > 0) s += ", ";
+			s += this.get_v(x, y, 15);
+		};
+		s += "\n";
+	};
+	console.log(s);
+};
+
+HPPGRID.prototype.print_density = function() {
+	var s = '';
+	for (var y = 0; y < this.height*2; y++) {
+		for (var x = 0; x < this.width*2; x++) {
+			s += this.get_density(x, y);
+		};
+		s += "\n";
+	};
+	console.log(s);
+};
--- scripts/hpp_sim.js
+++ scripts/hpp_sim.js
@@ -1,16 +1,22 @@
 var HPPSIM = function _HPPSIM(canvas) {
-	this.state = 0; //stopped
+	this.state = 0; 			// stopped
 	this.canvas = canvas[0];	
-
-
-	this.c_w = this.canvas.width;
-	this.c_h = this.canvas.height;
-
-	this.c_2d = this.canvas.getContext('2d');
-
-	this.imgdata = this.c_2d.createImageData(this.c_w, this.c_h);
-
 	this.tick = 0;
+
+	// pixels wide x height
+	this.groups_w = 250;
+	this.groups_h = 250;
+
+	// each pixel is really a 2x2 group
+	this.width = this.groups_w*2;
+	this.height = this.groups_h*2;
+
+	this.c = this.canvas.getContext('2d');
+	this.imgdata = this.c.createImageData(this.canvas.width, this.canvas.height);
+
+	this.cells = new Uint16Array(this.width * this.height);
+
+
 }
 
 HPPSIM.prototype.mainloop = function() {
@@ -22,7 +28,7 @@ HPPSIM.prototype.mainloop = function() {
 		this.imgdata.data[pi+1] = 10;
 		this.imgdata.data[pi+2] = 10;
 		this.imgdata.data[pi+3] = 255;
-		this.c_2d.putImageData(this.imgdata,0,0);
+		this.c.putImageData(this.imgdata,0,0);
 
 		this.tick += 1;
 		window.requestAnimFrame(HPPSIM.prototype.mainloop.bind(this));
